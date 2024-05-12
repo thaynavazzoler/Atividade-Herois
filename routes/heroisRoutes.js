40f5fff4106d11118a59ad7c69a4e1fc8a0833b7
thaynavazzoler <thaynavazzoler.564@gmail.com>
2024-05-12 16:19:35 -0300

    "Routes"

--- routes/heroisRoutes.js
+++ routes/heroisRoutes.js
@@ -7,5 +7,8 @@ router.get('/', heroisController.getAllHerois);
 router.post('/', heroisController.createHeroi);
 router.put('/:id', heroisController.updateHeroi);
 router.delete('/:id', heroisController.deleteHeroi);
+router.get('/:id', heroisController.getHeroiById);
+router.get('/nome/:nome', heroisController.getHeroisByNome);
+router.get('/poder/:poder', heroisController.getHeroisByPoder);
 
 module.exports = router;
