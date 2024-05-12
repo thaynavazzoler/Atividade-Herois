8d4ab25f3f2ad673854aef406b673cc2943e7cdd
thaynavazzoler <thaynavazzoler.564@gmail.com>
2024-05-12 14:12:53 -0300

    "Create update heroisController.js"

--- controllers/heroisController.js
+++ controllers/heroisController.js
@@ -23,3 +23,22 @@ async function getAllHerois(req, res) {
     res.status(500).send('Erro ao obter herois');
   }
 }
+
+async function updateHeroi(req, res) {
+  const id = req.params.id;
+  const { nome, poder, nivel, pontosdevida, ataque, defesa } = req.body;
+  const query = 'UPDATE herois SET nome=$1, poder=$2, nivel=$3, pontosdevida=$4, ataque=$5, defesa=$6 WHERE id=$7';
+  const values = [nome, poder, nivel, pontosdevida, ataque, defesa, id];
+
+  try {
+    const result = await pool.query(query, values);
+    if (result.rowCount > 0) {
+      res.send('Heroi atualizado com sucesso');
+    } else {
+      res.status(404).send('Heroi não encontrado');
+    }
+  } catch (err) {
+    console.error('Erro ao atualizar heroi:', err);
+    res.status(500).send('Erro ao atualizar heroi');
+  }
+}
